0d6467361e4d1bd0efb8f944cb0fae6829f5693c
charanbasam-25 <be.charankumar@gmail.com>
2025-01-08 00:44:07 +0530

    server notification changes

--- backend/server.js
+++ backend/server.js
@@ -46,7 +46,6 @@ if (process.env.NODE_ENV === "production") {
 		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
 	});
 }
-
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`);
 	connectMongoDB();
